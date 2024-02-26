7e634d5e90141af8ccc46a35a06e2933bb99d658
xQuarz <stefanie-schreiner@hotmail.de>
2024-02-26 06:45:49 +0100

    Make response type of vod_offset nullable

--- Projects/TwitchApi/TwitchApi/PoC/Assets/correct-header-in-yaml-file.ps1
+++ Projects/TwitchApi/TwitchApi/PoC/Assets/correct-header-in-yaml-file.ps1
@@ -3,7 +3,25 @@ function correct_header
     param([string]$filepath)
     $toFind = "client_id"
     $replacement = "Client-Id"
-    (Get-Content $filepath).Replace($toFind, $replacement) | Set-Content "%CD%\Assets\twitch-api.yaml"
+    (Get-Content $filepath).Replace($toFind, $replacement) | Set-Content $filepath
 }
 
-correct_header $args[0]
+
+function add_nullable_to_get_clips_response_vod_offset
+{
+    param([string]$filepath)
+    $regex = '(?<=vod_offset:\s+?type:\s)number'
+    $replacement = "`r`n                            - number`r`n                            - 'null'"
+    $content = Get-Content $filepath -Raw -Encoding utf8bom
+    $changedType = $content -Replace $regex, $replacement | Set-Content $filepath
+    $changedType -Replace "(?s)\r\n*$" 
+}
+
+$filepath = $args[0]
+if (!$args[0])
+{
+    $filepath = "twitch-api.yaml"
+}
+
+correct_header $filepath
+add_nullable_to_get_clips_response_vod_offset $filepath
